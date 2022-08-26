05d3c620b8223f504e23fedc964a8de730435ce1
tn-create1112 <0ak792332w3227j@au.com>
2022-08-26 13:22:38 +0900

    update aws settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,6 +9,8 @@ ALLOWED_HOSTS = ["*", ]
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = 'ap-northeast-1'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
@@ -17,7 +19,6 @@ S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = S3_URL
 
 # DB settings
-
 DATABASES = {
     'default': env.db()
 }
