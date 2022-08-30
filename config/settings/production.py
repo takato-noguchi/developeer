4830d72b596ee9e17c820f6ab3e635152f3bcce2
tn-create1112 <0ak792332w3227j@au.com>
2022-08-30 23:41:46 +0900

    update load static files settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -16,8 +16,6 @@ AWS_S3_SIGNATURE_VERSION = os.environ.get('AWS_S3_SIGNATURE_VERSION')
 AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
-
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = S3_URL
 
