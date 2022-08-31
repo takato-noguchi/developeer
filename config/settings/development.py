f992ab86b29b48c2ad006eb2d6054ca8c0f8f6ab
tn-create1112 <0ak792332w3227j@au.com>
2022-08-31 13:28:30 +0900

    update load static files settings

--- config/settings/development.py
+++ config/settings/development.py
@@ -151,6 +151,8 @@ STATICFILES_DIRS = [
 # STATIC FILES Settings
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
--- config/settings/production.py
+++ config/settings/production.py
@@ -9,13 +9,12 @@ ALLOWED_HOSTS = ["*", ]
 # AWS settings
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = 'developeer-storage'
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 AWS_S3_SIGNATURE_VERSION = os.environ.get('AWS_S3_SIGNATURE_VERSION')
 
 AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = S3_URL
