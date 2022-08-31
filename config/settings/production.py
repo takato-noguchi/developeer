4da7dc22e13cb3ea967aaa24abf40c361b483c77
tn-create1112 <0ak792332w3227j@au.com>
2022-08-31 22:27:23 +0900

    check aws settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,9 +7,9 @@ DEBUG = False
 ALLOWED_HOSTS = ["*", ]
 
 # AWS settings
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = 'AKIA3WIDXO7BR2GOJ6V6'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'developeer-storage'
 
 AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
