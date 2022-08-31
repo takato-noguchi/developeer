1924e607ff1eebb78df8bf6a4e5e69484a4baf9c
tn-create1112 <0ak792332w3227j@au.com>
2022-08-31 22:15:43 +0900

    check aws settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,11 +11,10 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-AWS_LOCATION = 'media'
+AWS_LOCATION = 'media_files'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_URL = S3_URL
 
 # DB settings
 DATABASES = {
